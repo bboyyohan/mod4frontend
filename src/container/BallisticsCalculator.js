efe870f76856f15923b214bc572bf1a09bb9bf78
thedesertfox2 <alexander.gautherin@uconn.edu>
2020-07-02 18:26:16 -0400

    Co-authored-by: Yo <yo.park93@yahoo.com>

--- src/container/BallisticsCalculator.js
+++ src/container/BallisticsCalculator.js
@@ -4,8 +4,16 @@ import Calibre from './Calibre'
 class BallisticsCalculator extends React.Component {
     render(){
         return(
-            <div> Ballistics Calculator
-                <Calibre />
+            <div> 
+                <select>
+                    {this.props.bulletType.map(bullet => <option value={bullet.calibre}>{bullet.calibre}</option>)}
+                </select>
+
+                <form>
+
+                        <input placeholder='Weight' value={this.props.bulletType.weight}/>
+                        <input type="Submit" value="Submit Calculations"/>
+                </form>
             </div>
 
         )
--- src/container/ContentContainer.js
+++ src/container/ContentContainer.js
@@ -3,10 +3,35 @@ import BallisticsCalculator from './BallisticsCalculator'
 
 
 class ContentContainer extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            bulletType: [],
+            guns: []
+        }
+    }
+
+    componentDidMount(){
+        this.fetchBulletType()
+        this.fetchGuns()
+    }
+
+    fetchGuns = () => {
+        fetch('http://localhost:3000/guns')
+        .then(res => res.json())
+        .then(allGunsData => this.setState ({guns: allGunsData}))
+    }
+
+    fetchBulletType = () => {
+        fetch("http://localhost:3000/bullet_types").then(res => res.json()).then(bulletTypeData => this.setState({
+            bulletType: bulletTypeData
+        }))
+    }
+
     render(){
         return(
             <div>Hello World
-                <BallisticsCalculator />
+                <BallisticsCalculator guns={this.state.guns} bulletType={this.state.bulletType}/>
             </div>
         )
     }
