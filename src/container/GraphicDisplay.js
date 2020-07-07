3b3e0649f8493ef29502a00425113797927cdc20
thedesertfox2 <alexander.gautherin@uconn.edu>
2020-07-07 17:11:33 -0400

    start of the canvas

--- src/container/GraphicDisplay.js
+++ src/container/GraphicDisplay.js
@@ -1,14 +1,38 @@
 import React from 'react'
+import CanvasDraw from "react-canvas-draw";
 
 class GraphicDisplay extends React.Component {
+
+    state ={
+        brushRadius: 3,
+        lazyRadius: 0
+    }
+
     render(){
         return(
             <div> GraphicDisplay
-               
+               <CanvasDraw lazyRadius={this.state.lazyRadius} brushRadius={this.state.brushRadius} onChange={() => console.log("onChange")} />
             </div>
 
         )
     }
 }
+
+GraphicDisplay.defaultProps = {
+    onChange: null,
+    loadTimeOffset: 5,
+    lazyRadius: 30,
+    brushColor: "#444",
+    catenaryColor: "#0a0302",
+    gridColor: "rgba(150,150,150,0.17)",
+    hideGrid: false,
+    canvasWidth: 400,
+    canvasHeight: 400,
+    disabled: false,
+    imgSrc: "",
+    saveData: null,
+    immediateLoading: false,
+    hideInterface: false
+  };
 
 export default GraphicDisplay
